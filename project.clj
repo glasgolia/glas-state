fac6043ceab572a6a5c9625fa5e1b939eddb784b
Peter Muys <peter@petermuys.com>
2019-10-10 22:23:01 +0200

    project info settings updated

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject glas-state "0.1.0-SNAPSHOT"
-  :description "Statecharts for clojure and clojurescript"
+(defproject glas-state "0 - Alpha"
+  :description "Statecharts for clojure and clojurescript,"
   :url "http://glasgolia.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
